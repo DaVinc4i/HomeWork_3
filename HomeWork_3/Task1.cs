c084e254b6097ecb6bb629d6bd0cc13f29c1e8b9
Da_Vinc4i <da_vinc4i_play@gmail.com>
2022-08-15 05:57:24 +0300

    My First Commit

---
+++ HomeWork_3/Task1.cs
@@ -0,0 +1,383 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HomeWork; //добавление библиотеки (пространства HomeWork) из предыдущей домашней работы
+
+
+namespace HomeWork_3
+{
+    //1. а) Дописать структуру Complex, добавив метод вычитания комплексных чисел.Продемонстрировать работу структуры.
+    //б) Дописать класс Complex, добавив методы вычитания и произведения чисел.Проверить работу класса.
+    //в) Добавить диалог с использованием switch демонстрирующий работу класса.
+
+    // решение предоставил Юрченко Н.А.
+
+    public struct Complex
+    {
+        /// <summary>
+        /// Мнимая часть комплексногоч исла
+        /// </summary>
+        public double im;
+
+        /// <summary>
+        /// Действительная часть комплексногоч исла
+        /// </summary>
+        public double re;
+
+        /// <summary>
+        /// Метод реализации сложения комплексных чисел
+        /// </summary>
+        /// <param name="x">Комплексное число для сложения</param>
+        /// <returns></returns>
+        public Complex Plus(Complex x)
+        {
+            Complex y = new Complex();
+            y.re = re + x.re;
+            y.im = im + x.im;
+            return y;
+        }
+
+        /// <summary>
+        /// Метод реализации сложения комплексных чисел (другая реализация)
+        /// </summary>
+        /// <param name="complex1">Первое комплексное число для сложения</param>
+        /// <param name="complex2">Второе комплексное число для сложения</param>
+        /// <returns></returns>
+        public static Complex Plus(Complex complex1, Complex complex2)
+        {
+            Complex res = new Complex();
+            res.re = complex1.re + complex2.re;
+            res.im = complex1.im + complex2.im;
+            return res;
+        }
+
+        /// <summary>
+        /// Метод реализации вычитания комплексных чисел 
+        /// </summary>
+        /// <param name="x">Комплексное число, которое отнимаем</param>
+        /// <returns></returns>
+        public Complex Minus (Complex x)
+        {
+            Complex y = new Complex();
+            y.re = re - x.re;
+            y.im = im - x.im;
+            return y;
+        }
+
+        /// <summary>
+        /// Метод реализации вычитания комплексных чисел (другая реализация)
+        /// </summary>
+        /// <param name="complex1">Комплексное число, от которого отнимаем</param>
+        /// <param name="complex2">Комплексное число, которое отнимаем</param>
+        /// <returns></returns>
+        public static Complex Minus (Complex complex1, Complex complex2)
+        {
+            Complex res = new Complex();
+            res.re = complex1.re - complex2.re;
+            res.im = complex1.im - complex2.im;
+            return res;
+        }
+
+        public override string ToString()
+        {
+            return $"{re} + {im}i";
+        }
+
+
+    }
+
+    public class ComplexClass
+    {
+        /// <summary>
+        /// Мнимая часть комплексногоч исла
+        /// </summary>
+        public double im;
+
+        /// <summary>
+        /// Действительная часть комплексногоч исла
+        /// </summary>
+        public double re;
+
+        /// <summary>
+        /// Конструктор класса
+        /// </summary>
+        /// <param name="re">Действительная часть комплексногоч исла</param>
+        /// <param name="im">Мнимая часть комплексногоч исла</param>
+        public ComplexClass(double re, double im)
+        {
+            this.re = re;
+            this.im = im;
+        }
+
+        /// <summary>
+        /// Метод реализации сложения комплексных чисел 
+        /// </summary>
+        /// <param name="x">Комплексное число для сложения</param>
+        /// <returns></returns>
+        public ComplexClass Plus(ComplexClass x)
+        {
+            ComplexClass y = new ComplexClass(0,0);
+            y.re = re + x.re;
+            y.im = im + x.im;
+            return y;
+        }
+
+        /// <summary>
+        /// Метод реализации сложения комплексных чисел (другая реализация)
+        /// </summary>
+        /// <param name="complex1"></param>
+        /// <param name="complex2"></param>
+        /// <returns></returns>
+        public static ComplexClass Plus(ComplexClass complex1, ComplexClass complex2)
+        {
+            ComplexClass res = new ComplexClass(0,0);
+            res.re = complex1.re + complex2.re;
+            res.im = complex1.im + complex2.im;
+            return res;
+        }
+        /// <summary>
+        /// Метод реализации вычитания комплексных чисел
+        /// </summary>
+        /// <param name="x">Вычитаемое комплексное число</param>
+        /// <returns></returns>
+        public ComplexClass Minus(ComplexClass x)
+        {
+            ComplexClass y = new ComplexClass(0, 0);
+            y.re = re - x.re;
+            y.im = im - x.im;
+            return y;
+        }
+
+        /// <summary>
+        /// Метод реализации вычитания комплексных чисел (другая реализация)
+        /// </summary>
+        /// <param name="complex1">Комплексное число, от которого вычитаем</param>
+        /// <param name="complex2">Вычитаемое комплексное число</param>
+        /// <returns></returns>
+        public static ComplexClass Minus(ComplexClass complex1, ComplexClass complex2)
+        {
+            ComplexClass res = new ComplexClass(0,0);
+            res.re = complex1.re - complex2.re;
+            res.im = complex1.im - complex2.im;
+            return res;
+        }
+
+        /// <summary>
+        /// Метод реализации вычитания комплексных чисел
+        /// </summary>
+        /// <param name="x">Комплексное число</param>
+        /// <returns></returns>
+        public ComplexClass Multiplication(ComplexClass x)
+        {
+            ComplexClass y = new ComplexClass(0, 0);
+            y.re = re * x.re-im*x.im;
+            y.im = re * x.im+x.re*im;
+            return y;
+        }
+
+        /// <summary>
+        /// Метод реализации вычитания комплексных чисел (другая реализация)
+        /// </summary>
+        /// <param name="complex1">Первое комплексное число</param>
+        /// <param name="complex2">Второе комплексное число</param>
+        /// <returns></returns>
+        public static ComplexClass Multiplication(ComplexClass complex1, ComplexClass complex2)
+        {
+            ComplexClass res = new ComplexClass(0, 0);
+            res.re = complex1.re * complex2.re- complex1.im * complex2.im;
+            res.im = complex1.re * complex2.im+ complex1.im * complex2.re;
+            return res;
+        }
+
+        public override string ToString()
+        {
+            return $"{re} + {im}i";
+        }
+
+    }
+
+    class Task1
+    {
+        /// <summary>
+        /// Метод выводящий разделительную прямую линию на экране консоли
+        /// </summary>
+        public static void Line()
+        {
+            int i = 0;//переменная для счетчика вывода прямой линии
+
+            while (i != Console.WindowWidth)
+            {
+                Console.Write("-");
+                i++;
+            }
+
+            Console.WriteLine();
+
+        }
+
+        static void Main(string[] args)
+        {
+            var screen = new Screen();
+            screen.ScreenPrint(3, 1);
+            // Вызов метода ScreenPrint c предыдущей домашней работы, для вывода номера задания 
+            // и домашней работы, а также вывода ФИО выполнившего студента
+
+            #region Демонстрация работы сложения и вычитания структуры
+
+            Console.WriteLine();
+            Console.WriteLine("Демонстрация сложения и вычитания структуры");
+
+            Complex complex1 = new Complex();
+            complex1.re = 3;
+            complex1.im = -2;
+
+            Complex complex2 = new Complex();
+            complex2.re = 1;
+            complex2.im = 3;
+
+            Console.WriteLine($"Работа с комплексными числами {complex1} и {complex2}");
+
+            Complex complex3 = complex1.Plus(complex2);
+
+            Console.WriteLine($"Результат сложения комплексных чисел: {complex3}");
+
+            Complex complex4 = Complex.Plus(complex2, complex1);
+
+            Console.WriteLine($"Результат сложения комплексных чисел: {complex4}");
+
+            Complex complex13 = complex1.Minus(complex2);
+
+            Console.WriteLine($"Результат вычитания комплексных чисел: {complex13}");
+
+            Complex complex14 = Complex.Minus(complex1, complex2);
+
+            Console.WriteLine($"Результат вычитания комплексных чисел: {complex14}");
+
+            Line();
+
+            #endregion
+
+            #region Демонстрация сложения, вычитания и умножения класса
+
+
+            Console.WriteLine("Демонстрация сложения, вычитания и умножения класса");
+
+            ComplexClass complex5 = new ComplexClass(3, -2);
+
+            ComplexClass complex6 = new ComplexClass(1, 3);
+           
+            Console.WriteLine($"Работа с комплексными числами {complex5} и {complex6}");
+
+            ComplexClass complex7 = complex5.Plus(complex6);
+
+            Console.WriteLine($"Результат сложения комплексных чисел: {complex7}");
+
+            ComplexClass complex8 = ComplexClass.Plus(complex5, complex6);
+
+            Console.WriteLine($"Результат сложения комплексных чисел: {complex8}");
+           
+            ComplexClass complex9 = complex5.Minus(complex6);
+
+            Console.WriteLine($"Результат вычитания комплексных чисел: {complex9}");
+
+            ComplexClass complex10 = ComplexClass.Minus(complex5, complex6);
+
+            Console.WriteLine($"Результат вычитания комплексных чисел: {complex10}");
+
+            ComplexClass complex11 = complex5.Multiplication(complex6);
+
+            Console.WriteLine($"Результат умножения комплексных чисел: {complex11}");
+
+            ComplexClass complex12 = ComplexClass.Multiplication(complex5, complex6);
+
+            Console.WriteLine($"Результат умножения комплексных чисел: {complex12}");
+
+            Line();
+
+            #endregion
+
+            #region Добавление диалога с использованием switch демонстрирующий работу класса.
+
+            Console.WriteLine("Демонстрация работы класса с использованием диалога switch");
+
+            var complexOne = new ComplexClass(0,0);
+            var complexTwo = new ComplexClass(0,0);
+
+            Console.Write("Введите действительную часть первого комплексного числа: ");
+            complexOne.re = double.Parse(Console.ReadLine());
+
+            Console.Write("Введите мнимую часть первого комплексного числа: ");
+            complexOne.im = double.Parse(Console.ReadLine());
+
+            Console.Write("Введите действительную часть второго комплексного числа: ");
+            complexTwo.re = double.Parse(Console.ReadLine());
+
+            Console.Write("Введите мниную часть второго комплексного числа: ");
+            complexTwo.im = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Работа с комплексными числами {complexOne} и {complexTwo}");
+
+            Console.WriteLine();
+
+            string menu;
+
+            do
+            {
+                Console.WriteLine("Нажмите клавишу действия для комплексных числе:");
+                Console.WriteLine("+ - для сложения комплексных чисел между собой");
+                Console.WriteLine("- - для вычитания комплексных чисел");
+                Console.WriteLine("* - для перемножения комплексных чисел");
+                Console.WriteLine("0 - для завершения программы");
+                Console.WriteLine();
+
+                menu = Console.ReadLine();
+
+                switch (menu)
+                {
+                    case "-":                        
+                        ComplexClass complexResult = complexOne.Minus(complexTwo);
+                        Console.WriteLine();
+                        Console.WriteLine($"Результат вычитания комплексных чисел: {complexResult}");
+                        Console.WriteLine();
+                        break;
+
+                    case "+":                        
+                        complexResult = complexOne.Plus(complexTwo);
+                        Console.WriteLine();
+                        Console.WriteLine($"Результат сложения комплексных чисел: {complexResult}");
+                        Console.WriteLine();
+                        break;
+
+                    case "*":                        
+                        complexResult = complexOne.Multiplication(complexTwo);
+                        Console.WriteLine();
+                        Console.WriteLine($"Результат вычитания комплексных чисел: {complexResult}");
+                        Console.WriteLine();
+                        break;
+
+                    case "0":
+                        Console.WriteLine();
+                        Console.WriteLine("Действие программы завершено. До свидания!");
+                        Console.WriteLine();
+                        break;
+
+
+                    default:
+                        Console.WriteLine();
+                        Console.WriteLine("Набран неправильный символ на клавиатуре. Повторите ввод!");
+                        Console.WriteLine();
+                        break;
+                }
+            }
+            while (menu != "0");
+            
+            Console.ReadLine();
+
+            #endregion
+
+        }
+    }
+}
+
---
+++ HomeWork_3/Task2.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HomeWork;
+
+namespace HomeWork_3
+{
+    //2. а) С клавиатуры вводятся числа, пока не будет введён 0 (каждое число в новой строке).
+    //Требуется подсчитать сумму всех нечётных положительных чисел.
+    //Сами числа и сумму вывести на экран, используя tryParse.
+
+    // решение предоставил Юрченко Н.А.
+
+    class Task2
+    {
+        /// <summary>
+        /// Метод создающий массив 
+        /// </summary>
+        /// <param name="array">Изначальный массив данных</param>
+        /// <param name="number">Введеное число</param>
+        /// <returns></returns>
+        static void CreateArray(ref int [] array, int number)
+        {
+            int [] myArray = new int[array.Length+1];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                myArray[i] = array[i];
+            }
+            myArray[array.Length] = number;
+
+            array = myArray;
+        }
+
+        /// <summary>
+        /// Метод проверяющий и выводящий на экран нечетные числа массива
+        /// </summary>
+        /// <param name="array">Массив данных для перебора чисел</param>
+        static void PrintOdd(int [] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] % 2 != 0)
+                    Console.Write("  " + array[i]);
+            }
+            Console.WriteLine();
+        }
+
+        /// <summary>
+        /// Метод проверяющий и суммирующий нечетные числа массива
+        /// </summary>
+        /// <param name="array">Массив данных для перебора и суммирования чисел</param>
+        static int SummOdd(int[] array)
+        {
+            int summ=0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] % 2 != 0)
+                     summ+= array[i];
+            }
+            return summ;
+        }
+
+        /// <summary>
+        /// Метод проверяющий и суммирующий числа
+        /// </summary>
+        /// <param name="number">Введенное число</param>
+        /// <param name="summOdd">Сумма нечечтных чисел</param>
+        /// <returns></returns>
+        static int SummOdd(int number, int summOdd)
+        {   
+            if (number % 2 != 0)
+                    summOdd += number;
+            
+            return summOdd;
+        }
+
+        static string NumberOdd(int number, string numberOdd)
+        {
+            if (number % 2 != 0)
+                numberOdd += number+" ";
+
+            return numberOdd;
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            var screen = new Screen();
+            screen.ScreenPrint(3, 2);
+            // Вызов метода ScreenPrint c предыдущей домашней работы, для вывода номера задания 
+            // и домашней работы, а также вывода ФИО выполнившего студента
+
+            Console.WriteLine("Вводите целые числа для подсчета суммы всех положительных нечётных чисел");
+            Console.WriteLine("Для вывода на экран суммы и самих чисел, нажмите 0");
+            Console.WriteLine();
+
+            #region Решение задачи с использованием массива
+            Console.WriteLine("Решение через массив");
+            Console.WriteLine();
+
+            string numberStr;
+            int number;            
+
+            int [] array = new int [0];
+
+            do
+            {
+                Console.Write("Введите число:  ");
+                numberStr = Console.ReadLine();
+
+                bool result = int.TryParse(numberStr, out number);
+
+                if (result)
+                {
+                    if (number == 0)
+                        break;
+                    else
+                        CreateArray(ref array, number);
+                }
+                else
+                {
+                    Console.WriteLine("Ошибка ввода. Повторите ввод");
+                    number = -1;
+                }
+
+            }
+            while (number != 0);
+
+            Console.WriteLine();
+            Console.Write("Введенные нечетные числа: ");
+
+            PrintOdd(array);
+            Console.WriteLine("Сумма введенных нечетных чисел равна: "+SummOdd(array));
+
+            #endregion
+
+            #region Решение через строку и сумму        
+            
+            Console.WriteLine();
+            Console.WriteLine("Решение через строку и сумму");
+            Console.WriteLine();
+
+            int summOdd=0;
+            string numberOdd=null;
+
+            do
+            {
+                Console.Write("Введите число:  ");
+                numberStr = Console.ReadLine();
+
+                bool result = int.TryParse(numberStr, out number);
+
+                if (result)
+                {
+                    if (number == 0)
+                        break;
+                    else
+                    {
+                        summOdd = SummOdd(number, summOdd);
+                        numberOdd = NumberOdd(number, numberOdd);
+                    }
+
+                }
+                else
+                {
+                    Console.WriteLine("Ошибка ввода. Повторите ввод");
+                    number = -1;
+                }
+
+            }
+            while (number != 0);
+
+            Console.WriteLine();
+            Console.WriteLine("Введенные нечетные числа: "+numberOdd);
+
+            
+            Console.WriteLine("Сумма введенных нечетных чисел равна: " + summOdd);
+
+            #endregion
+
+            Console.ReadLine();
+
+
+        }
+
+    }
+}
